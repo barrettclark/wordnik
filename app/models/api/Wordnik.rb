361673a095ee6876b0da4c962ef9abfcfbe8c893
Barrett Clark <barrett@websitesbybarrett.com>
2010-05-01 20:59:51 -0500

    ActiveResource works!

--- app/models/api/Wordnik.rb
+++ app/models/api/Wordnik.rb
@@ -1,14 +1,12 @@
 module Api
   class Wordnik < ActiveResource::Base
-    self.site = "http://api.wordnik.com/api"
+    self.site = "http://api.wordnik.com/"
     self.timeout = 20
-    self.format = :json
-    headers['api_key'] = 'my_key'
+    headers['api_key'] = 'my_api_key'
 
     def self.word_of_the_day
       # wod = Api::Wordnik.word_of_the_day
-      self.find(:one, :from => "/wordoftheday.json")
-      # self.find(:one, :from => :wordoftheday)
+      self.find(:one, :from => "/api/wordoftheday.xml")
     end
   end
 end
---
+++ config/initializers/console.rb
@@ -0,0 +1,6 @@
+# make AR output go to STDOUT in script/console
+if "irb" == $0
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
+  ActiveResource::Base.logger = Logger.new(STDOUT)
+end
+ActiveResource::Base.logger = ActiveRecord::Base.logger
