71ec49663356c2342c19dde3f6dc6841340d9e80
Barrett Clark <barrett@websitesbybarrett.com>
2010-05-08 23:03:20 -0500

    remove ActiveRecord dependency from Rails 3

--- config/initializers/script_console_logging.rb
+++ config/initializers/script_console_logging.rb
@@ -1,6 +1,6 @@
 # make AR output go to STDOUT in script/console
 if "irb" == $0
-  ActiveRecord::Base.logger = Logger.new(STDOUT)
+  # ActiveRecord::Base.logger = Logger.new(STDOUT)
   ActiveResource::Base.logger = Logger.new(STDOUT)
 end
-ActiveResource::Base.logger = ActiveRecord::Base.logger
+# ActiveResource::Base.logger = ActiveRecord::Base.logger
