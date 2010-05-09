ce629d751d375afa38aeaec446037536690e1fd0
Barrett Clark <barrett@websitesbybarrett.com>
2010-05-08 23:00:28 -0500

    removed redundant initializer

--- config/initializers/console.rb
+++
@@ -1,6 +0,0 @@
-# make AR output go to STDOUT in script/console
-if "irb" == $0
-  ActiveRecord::Base.logger = Logger.new(STDOUT)
-  ActiveResource::Base.logger = Logger.new(STDOUT)
-end
-ActiveResource::Base.logger = ActiveRecord::Base.logger
