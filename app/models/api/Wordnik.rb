eb76d6a6d4e24cc969b714b13d554a9a7ded639b
Barrett Clark <barrett@websitesbybarrett.com>
2010-05-24 23:23:56 -0500

    word lookup api and changed to JSON

--- app/models/api/Wordnik.rb
+++ app/models/api/Wordnik.rb
@@ -2,21 +2,27 @@ module Api
   class Wordnik < ActiveResource::Base
     self.site = "http://api.wordnik.com/"
     self.timeout = 20
+    self.format = :json
     headers['api_key'] = '93258932a2cf8104dce2604abe90708fa097c4d18b9a26e52'
+
+    def self.definitions(word, count = 1)
+      # word = Api::Wordnik.definitions(word)
+      self.find(:all, :from => "/api/word.json/#{word}/definitions?count=#{count}")
+    end
 
     def self.random_word
       # random = Api::Wordnik.random_word
-      word = self.find(:one, :from => "/api/words.xml/randomWord?hasDictionaryDef=true")
+      word = self.find(:one, :from => "/api/words.json/randomWord?hasDictionaryDef=true")
     end
     
+    def self.suggest(word)
+      # words = Api::Wordnik.suggest(word)
+      self.find(:one, :from => "/api/word.json/#{word}?useSuggest=true&literal=false")
+    end
+
     def self.word_of_the_day
       # wod = Api::Wordnik.word_of_the_day
-      self.find(:one, :from => "/api/wordoftheday.xml")
-    end
-    
-    def self.definitions(word, count = 1)
-      # word = Api::Wordnik.definitions(word)
-      self.find(:one, :from => "/api/word.xml/#{word}/definitions?count=#{count}")
+      self.find(:one, :from => "/api/wordoftheday.json")
     end
   end
 end
